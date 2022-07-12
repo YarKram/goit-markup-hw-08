948d20b757e898b320780e9560efe2f927d158c3
Yaroslav <yaroslav.kramarchuk@gmail.com>
2022-07-12 15:06:46 +0200

    pictures and mobile menu added

---
+++ js/menu.js
@@ -0,0 +1,44 @@
+// (() => {
+// 	const refs = {
+// 		openMenuBtn: document.querySelector("[data-menu-open]"),
+// 		closeMenuBtn: document.querySelector("[data-menu-close]"),
+// 		menu: document.querySelector("[data-menu]"),
+// 		body: document.querySelector("body"),
+// 	};
+// 	refs.openMenuBtn.addEventListener("click", toggleMenu);
+// 	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+// 	function toggleMenu() {
+// 		refs.menu.classList.toggle("is-hidden");
+// 		refs.body.classList.toggle("no-scroll");
+// 	}
+// })();
+
+// (() => {
+// 	const refs = {
+// 		openMenuBtn: document.querySelector("[mob-menu__button]"),
+// 		closeMenuBtn: document.querySelector("[mob-menu__close]"),
+// 		menu: document.querySelector("[mob-menu]"),
+// 		body: document.querySelector("body"),
+// 	};
+// 	refs.openMenuBtn.addEventListener("click", toggleMenu);
+// 	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+// 	function toggleMenu() {
+// 		refs.menu.classList.toggle("is-hidden");
+// 		refs.body.classList.toggle("no-scroll");
+// 	}
+// })();
+
+(() => {
+	const refs = {
+		openMenuBtn: document.querySelector(".mob-menu__button"),
+		closeMenuBtn: document.querySelector(".mob-menu__close"),
+		menu: document.querySelector(".mob-menu"),
+		body: document.querySelector("body"),
+	};
+	refs.openMenuBtn.addEventListener("click", toggleMenu);
+	refs.closeMenuBtn.addEventListener("click", toggleMenu);
+	function toggleMenu() {
+		refs.menu.classList.toggle("is-hidden");
+		refs.body.classList.toggle("no-scroll");
+	}
+})();
